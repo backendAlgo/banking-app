ea7a3a91ec231bd024b884ae8aee838706aa4a4d
mirshod <mmirjonov@mwiah.co.uk>
2023-06-09 17:25:09 +0500

    jdbcuserdetailsmanager example

--- src/main/java/uz/najottalim/bankingapp/config/SecurityConfig.java
+++ src/main/java/uz/najottalim/bankingapp/config/SecurityConfig.java
@@ -2,17 +2,14 @@ package uz.najottalim.bankingapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.ui.DefaultLoginPageGeneratingFilter;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,8 +18,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 //        DefaultLoginPageGeneratingFilter
-        AuthenticationManagerBuilder
-                DaoAuthenticationProvider
+//        AuthenticationManagerBuilder
+//                DaoAuthenticationProvider
         http.authorizeHttpRequests(
                 (requests) ->
                         requests.requestMatchers(
@@ -40,25 +37,34 @@ public class SecurityConfig {
         );
 //        UserDetailsManager
 //        UserDetailsService
+//        DaoAuthenticationProvider
+//        ProviderManager
+//        UsernamePasswordAuthenticationFilter
+//        UserDetailsService
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
-
     @Bean
-    public UserDetailsService myCustomerUserDetailsManager() {
-        UserDetails userDetails1 = User.builder().username("mirshod")
-                .password("12345")
-                .build();
-        UserDetails userDetails2 = User.builder().username("sherzod")
-                .password("12345")
-                .build();
-//        DaoAuthenticationProvider
-        InMemoryUserDetailsManager inMemoryUserDetailsManager =
-                new InMemoryUserDetailsManager(userDetails1, userDetails2);
-//        DaoAuthenticationProvider
-        return inMemoryUserDetailsManager;
+    public UserDetailsService springJdbcVersionForUserDetailsManager(DataSource source) {
+        return new JdbcUserDetailsManager(source);
     }
+
+
+//    @Bean
+//    public UserDetailsService myCustomerUserDetailsManager() {
+//        UserDetails userDetails1 = User.builder().username("mirshod")
+//                .password("12345")
+//                .build();
+//        UserDetails userDetails2 = User.builder().username("sherzod")
+//                .password("12345")
+//                .build();
+////        DaoAuthenticationProvider
+//        InMemoryUserDetailsManager inMemoryUserDetailsManager =
+//                new InMemoryUserDetailsManager(userDetails1, userDetails2);
+////        DaoAuthenticationProvider
+//        return inMemoryUserDetailsManager;
+//    }
     // 12345 -> hash12345
     // mirshod, bazadahash12345
 
